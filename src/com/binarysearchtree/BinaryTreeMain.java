9eafaea316d9e60ec0d99d647b7214e0d4bbdead
drisyaathul <drisyak809@gmail.com>
2023-08-14 21:36:42 +0800

    Added: create BST added nodes with using Size method

--- src/com/binarysearchtree/BinaryTreeMain.java
+++ src/com/binarysearchtree/BinaryTreeMain.java
@@ -9,6 +9,24 @@ public class BinaryTreeMain {
         binaryTree.add(70);
 
         binaryTree.display(binaryTree.root);
+        System.out.println("\n-------------------------------------------");
+
+        BinaryTree <Integer> binaryTree1 = new BinaryTree<>();
+        binaryTree1.add(56);
+        binaryTree1.add(30);
+        binaryTree1.add(70);
+        binaryTree1.add(22);
+        binaryTree1.add(40);
+        binaryTree1.add(60);
+        binaryTree1.add(95);
+        binaryTree1.add(11);
+        binaryTree1.add(65);
+        binaryTree1.add(3);
+        binaryTree1.add(16);
+        binaryTree1.add(63);
+        binaryTree1.add(67);
+
+        binaryTree.display(binaryTree1.root);
 
     }
 }
